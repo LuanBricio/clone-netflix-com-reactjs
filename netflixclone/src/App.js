77dbdf70ac2b966a6c7df251f7b8914dba76024b
LuanBricio <luanbricio0@gmail.com>
2024-09-12 20:40:30 -0300

    criando o Featured

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -2,23 +2,42 @@ import React, { useEffect, useState } from "react";
 import tmdb from './tmdb';
 import MovieRow from "./components/MovieRow";
 import './App.css';
+import FeaturedMovie from "./components/FeaturedMovie";
 
 export default () => {
 
 
     const [movieList, setMovieList] = useState([]);
+    const [FeaturedData, setFeaturedData] = useState(null);
 
     useEffect(() => {
         const loadAll = async() => {
             // pegando a lista TOTAL
             let list = await tmdb.getHomelist();
             setMovieList(list);
+
+            //pegando o Featured
+            let originals = list.filter(i => i.slug === 'originals');
+            let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+            let chosen = originals[0].items.results[randomChosen];
+            let chosenInfo = await tmdb.getMovieInfo(chosen.id, 'tv');
+
+            console.log(chosenInfo);
         }
 
         loadAll();
     }, []);
     return ( <
         div className = "page" >
+
+        {
+            FeaturedData &&
+            <
+            FeaturedMovie item = { FeaturedData }
+            />
+
+        }
+
         <
         section className = "lists" > {
             movieList.map((item, key) => ( <
@@ -28,7 +47,7 @@ export default () => {
                 />
             ))
         } <
-        /section> <
-        /div>
+        /section> < /
+        div >
     );
 }
