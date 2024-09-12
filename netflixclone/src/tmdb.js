c2fdeab0d0bdb16ca871be7bd25cec2a05b0a72f
LuanBricio <luanbricio0@gmail.com>
2024-09-12 13:24:04 -0300

    comecei a puxar os dados da api

---
+++ netflixclone/src/tmdb.js
@@ -0,0 +1,70 @@
+const API_KEY = '2413c1ea45dc0a12214a51cdd7fbbd63';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+
+/*
+- originais da netflix
+- recomendados (trending)
+- em alta (top rated)
+- ação
+- comédia
+- terror
+- romance
+- documentários
+*/
+
+
+const basicFetch = async(endpoint) => {
+    const req = await fetch(`${API_BASE}${endpoint}`);
+    const json = await req.json();
+    return json;
+}
+
+
+export default {
+    getHomeList: async() => {
+        return [{
+                slug: 'originals',
+                title: 'Originais do Netflix',
+                items: await basicFetch(`/discover/tv?with_network=213`)
+
+            },
+            {
+                slug: 'trending',
+                title: 'Recomendados para Você',
+                items: []
+            },
+            {
+                slug: 'toprated',
+                title: 'Em Alta',
+                items: []
+            },
+            {
+                slug: 'action',
+                title: 'Ação',
+                items: []
+            },
+            {
+                slug: 'comedy',
+                title: 'Comédia',
+                items: []
+            },
+            {
+                slug: 'horror',
+                title: 'Terror',
+                items: []
+            },
+            {
+                slug: 'romance',
+                title: 'Romance',
+                items: []
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentários',
+                items: []
+            },
+
+        ];
+    }
+}
